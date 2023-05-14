1581c4cc4317f1ccbbe1e488993385e8808b09a0
JongKI-PARK <97037853+JongKI-PARK@users.noreply.github.com>
2023-05-14 17:41:06 +0900

    Update install-docker.sh

--- Container/install-docker.sh
+++ Container/install-docker.sh
@@ -21,6 +21,8 @@ echo \
 # Installation - install docker engine
 sudo apt-get update
 
+sudo apt-cache policy docker-ce
+
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 sudo docker run hello-world
